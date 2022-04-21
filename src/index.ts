c5c0ce254465b4e254e58ac2954eeb87783b8ba8
inpatr <i.traub@posteo.de>
2022-04-21 16:59:07 +0200

    my changes

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,81 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
+import { Prisma, PrismaClient } from "@prisma/client";
+import express from "express";
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post(`/post/`, async (req, res) => {
-    const { author, email, title, content, image, tags } = req.body
+  const { author, title, content, image, tags } = req.body;
 
-    const result = await prisma.post.create({
-        data: {
-            author,
-            email, 
-            title, 
-            content, 
-            image,
-            tags,
-        },
-    })
+  const result = await prisma.post.create({
+    data: {
+      author,
+      title,
+      content,
+      image,
+      tags,
+    },
+  });
 
-    res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/post/', async (req, res) => {
+app.get("/post/", async (req, res) => {
+  const posts = await prisma.post.findMany({});
 
-    const posts = await prisma.post.findMany({})
+  res.json(posts);
+});
 
-    res.json(posts)
-})
+app.put("/post/:id/views", async (req, res) => {
+  const { id } = req.params;
 
-app.put('/post/:id/views', async (req, res) => {
-    const { id } = req.params
-
-    try {
+  try {
     const post = await prisma.post.update({
-        where: { id: Number(id) },
-        data: {
-            viewCount: {
-                increment: 1,
-            },
+      where: { id: Number(id) },
+      data: {
+        viewCount: {
+          increment: 1,
         },
-    })
+      },
+    });
 
-        res.json(post)
-    } catch (error) {
-        res.json({ error: `Post with ID ${id} does not exist in the database` })
-    }
-})
+    res.json("something else");
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
+});
 
 app.post(`/post/:post_id/comment/`, async (req, res) => {
-    const { post_id } = req.params;
-    const { author, email, content} = req.body
+  const { post_id } = req.params;
+  const { author, email, content } = req.body;
 
-    const result = await prisma.comment.create({
-        data: {
-            author,
-            email, 
-            content,
-            post: { connect: { id: Number(post_id) } }
-        },
-    })
+  const result = await prisma.comment.create({
+    data: {
+      author,
+      email,
+      content,
+      post: { connect: { id: Number(post_id) } },
+    },
+  });
 
-    res.json(result)
-})
+  res.json(result);
+});
 
-app.get('/post/:post_id/comment/', async (req, res) => {
-    const { post_id } = req.params;
-    const comments = await prisma.comment.findMany({
-        where: {
-            postId: Number(post_id)
-        }
-    })
+app.get("/post/:post_id/comment/", async (req, res) => {
+  const { post_id } = req.params;
+  const comments = await prisma.comment.findMany({
+    where: {
+      postId: Number(post_id),
+    },
+  });
 
-    res.json(comments)
-})
+  res.json(comments);
+});
 
 const server = app.listen(8000, () =>
-console.log(`
+  console.log(`
     🚀 Server ready at: http://localhost:8000
-    ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
-)
+    ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+);
